4ae108586a8e85c8d3f3fd1e6cea01f3817b5ed7
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2021-01-18 19:56:41 +0530

    check

--- src/Component/TaskList.js
+++ src/Component/TaskList.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import Task from './Task';
-import Options from './Options';
-const TaskList=(props)=>
-{
-	const {tasklist,deletetask,rerender}=props;
-	  return (
-	      tasklist.map((task,index)=>
-	      {
-	      	return <Task task={task.task} id={task.id} key={task.id} due={task.due} deletetask={deletetask} rerender={rerender}/>
-	      })
-	    );
-  }
+import React from "react";
+import Task from "./Task";
+import Options from "./Options";
+const TaskList = (props) => {
+	const { tasklist, deletetask, rerender } = props;
+	return tasklist.map((task, index) => {
+		return (
+			<Task
+				task={task.task}
+				id={task.id}
+				key={task.id}
+				due={task.due}
+				deletetask={deletetask}
+				rerender={rerender}
+			/>
+		);
+	});
+};
 
 export default TaskList;
-
-
-
-  	
